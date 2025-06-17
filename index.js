4c9604626a59128c3d836eddf5e7af074fc62ee4
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2025-06-17 19:52:57 +0300

    || 3000 is added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { resultsHandler } from './functions.js'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
